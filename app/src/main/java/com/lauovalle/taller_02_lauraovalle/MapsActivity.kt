1aaef9668047767a77c08fcdbfbea3f670555d57
Laura Ovalle Benítez <ovalle.laura@javeriana.edu.co>
2023-10-11 15:48:48 -0500

    primera op permisos mapas

--- app/src/main/java/com/lauovalle/taller_02_lauraovalle/MapsActivity.kt
+++ app/src/main/java/com/lauovalle/taller_02_lauraovalle/MapsActivity.kt
@@ -1,7 +1,13 @@
 package com.lauovalle.taller_02_lauraovalle
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -9,13 +15,36 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.snackbar.Snackbar
 import com.lauovalle.taller_02_lauraovalle.databinding.ActivityMapsBinding
+import java.util.logging.Logger
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
-
-    private lateinit var mMap: GoogleMap
+    // GLOBALES ----------------------------------------------------------------
     private lateinit var binding: ActivityMapsBinding
+    private lateinit var mMap: GoogleMap
+    companion object {
+        val TAG: String = cargar_imagen::class.java.name
+    }
 
+    private val logger = Logger.getLogger(TAG)
+
+    // Permission handler
+    @SuppressLint("MissingPermission")
+    private val getSimplePermission = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()) {
+        if(it)
+        {
+            logger.info("Permission granted")
+            mMap.isMyLocationEnabled = true
+        }
+        else
+        {
+            logger.info("Permission dennied")
+        }
+    }
+
+    // ON_CREATE ----------------------------------------------------------------
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -28,21 +57,42 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    // ON_CREATE ----------------------------------------------------------------
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+
+        // Pedir el permiso cuando la aplicación inicie
+        logger.info("Se va a solicitar el permiso")
+        verifyPermissions(this, android.Manifest.permission.ACCESS_FINE_LOCATION, "El permiso es requerido para...")
 
         // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    private fun verifyPermissions(context: Context, permission: String, rationale: String) {
+        when {
+            ContextCompat.checkSelfPermission(context, permission) == PackageManager.PERMISSION_GRANTED -> {
+                Snackbar.make(binding.root, "Ya tengo los permisos 😜", Snackbar.LENGTH_LONG).show()
+                logger.info("Permission granted")
+                mMap.isMyLocationEnabled = true
+            }
+            shouldShowRequestPermissionRationale(permission) -> {
+                // We display a snackbar with the justification for the permission, and once it disappears, we request it again.
+                val snackbar = Snackbar.make(binding.root, rationale, Snackbar.LENGTH_LONG)
+                snackbar.addCallback(object : Snackbar.Callback() {
+                    override fun onDismissed(snackbar: Snackbar, event: Int) {
+                        if (event == DISMISS_EVENT_TIMEOUT) {
+                            getSimplePermission.launch(permission)
+                        }
+                    }
+                })
+                snackbar.show()
+            }
+            else -> {
+                getSimplePermission.launch(permission)
+            }
+        }
     }
 }
